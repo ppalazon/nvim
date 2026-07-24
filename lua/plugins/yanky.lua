476675ad1e159de010778b850c3d89682c96ce69
Pablo Palazon <pablo.palazon@gmail.com>
2026-07-24 14:47:53 +0200

    Improve lua format files

--- lua/plugins/yanky.lua
+++ lua/plugins/yanky.lua
@@ -32,22 +32,10 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 
 -- Keymaps
 -- stylua: ignore start
-vim.keymap.set({ "n", "x" }, "<leader>y", function()
-  load_yanky(); require("snacks").picker.yanky()
-end, { desc = "Yank History" })
-vim.keymap.set({ "n", "x" }, "y", function()
-  load_yanky(); return "<Plug>(YankyYank)"
-end, { expr = true })
-vim.keymap.set({ "n", "x" }, "p", function()
-  load_yanky(); return "<Plug>(YankyPutAfter)"
-end, { expr = true })
-vim.keymap.set({ "n", "x" }, "P", function()
-  load_yanky(); return "<Plug>(YankyPutBefore)"
-end, { expr = true })
-vim.keymap.set("n", "<C-p>", function()
-  load_yanky(); return "<Plug>(YankyCycleForward)"
-end, { expr = true })
-vim.keymap.set("n", "<C-n>", function()
-  load_yanky(); return "<Plug>(YankyCycleBackward)"
-end, { expr = true })
+vim.keymap.set({ "n", "x" }, "<leader>yy", function() load_yanky(); require("snacks").picker.yanky() end, { desc = "Yank History" })
+vim.keymap.set({ "n", "x" }, "y", function() load_yanky(); return "<Plug>(YankyYank)" end, { expr = true })
+vim.keymap.set({ "n", "x" }, "p", function() load_yanky(); return "<Plug>(YankyPutAfter)" end, { expr = true })
+vim.keymap.set({ "n", "x" }, "P", function() load_yanky(); return "<Plug>(YankyPutBefore)" end, { expr = true })
+vim.keymap.set("n", "<C-p>", function() load_yanky(); return "<Plug>(YankyCycleForward)" end, { expr = true })
+vim.keymap.set("n", "<C-n>", function() load_yanky(); return "<Plug>(YankyCycleBackward)" end, { expr = true })
 -- stylua: ignore end

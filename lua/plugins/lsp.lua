d2ae6013cede5479ef159f44f52762e487566b2f
Pablo Palazon <pablo.palazon@gmail.com>
2026-07-28 10:07:36 +0200

    Add additional plugin with more slang-server features

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,3 +10,8 @@ require("mason-lspconfig").setup()
 require("mason-tool-installer").setup({
   ensure_installed = { "lua_ls" },
 })
+
+-- Install slang-server enhanced plugin, to start you can execute :SlangServer command
+vim.pack.add({
+  { src = "https://github.com/hudson-trading/slang-server.nvim" },
+})
